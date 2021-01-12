0219a10b8798765958ab9082621c79d0a324eb7b
shqiperonsadiku <74976264+shqiperonsadiku@users.noreply.github.com>
2021-01-12 14:46:34 +0100

    making a section functional..

--- contact.php
+++ contact.php
@@ -2,14 +2,15 @@
     $name = $_POST['name'];
     $visitor_email = $_POST['email'];
     $message = $_POST['message'];
+    $subject = $_POST['subject'];
 
     $email_from = 'HealthandFitness@info.com';
 
-    $email_subject = "New Form Submission";
+    $email_subject = $subject;
 
-    $email_body = "User Name: $name.\n".
-                    "User Email: $visitor_email.\n".
-                        "User Message: $message.\n";
+    $email_body = "User Name: $name\n".
+                    "User Email: $visitor_email\n".
+                        "User Message:\n $message.\n";
    
     $to = "healthandfitnessgr26@gmail.com";
     
@@ -18,6 +19,6 @@
     $headers .= "Reply-To: $visitor_email \r\n";
 
     mail($to,$email_subject,$email_body,$headers);
-
-    header("Location: projekti.html");
+    
+    header("Location: index.html");
 ?>
